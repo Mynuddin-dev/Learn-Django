900e5b9c87c04b314b90825ef5b96cfc4198d763
Mynuddin-dev <mdmynuddin.iit.se.nstu@gmail.com>
2024-04-18 12:54:11 +0600

    Add django-crispy form on blog project user registration

--- BlogProject/BlogProject/settings.py
+++ BlogProject/BlogProject/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'BlogApp.apps.BlogappConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
