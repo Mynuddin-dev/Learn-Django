5039b917220aa66340485e68b42a9e4cfd359599
Mynuddin-dev <mdmynuddin.iit.se.nstu@gmail.com>
2024-04-20 17:24:06 +0600

    Class Based views
    BlogApp home page funtion based home views write as a class based views

--- BlogProject/BlogApp/views.py
+++ BlogProject/BlogApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 def home(request):
@@ -6,6 +7,12 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'BlogApp/home.html' , context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'BlogApp/home.html'
+    context_object_name = 'posts'
+    # <app>/<model>_<viewtype>.html
 
 def about(request):
     context = {
